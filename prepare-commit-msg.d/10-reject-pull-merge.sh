77fce02e4e12985ad3214223092aa3bf538e6b6d
Ben Boeckel <ben.boeckel@kitware.com>
2012-08-06 21:50:31 -0400

    Simplify grepping the commit message

--- prepare-commit-msg.d/10-reject-pull-merge.sh
+++ prepare-commit-msg.d/10-reject-pull-merge.sh
@@ -1,4 +1,4 @@
-cat "$1" | head -n1 | grep -q -e "Merge branch \'.*\' of .*" &&
+head -n 1 "$1" | grep -q -e "Merge branch \'.*\' of .*" &&
 	die "Non-fast forward merges when pulling is not allowed.\n" \
 	    "Pull then remerge or set branch.<branch>.rebase = true."
 
