46b323d6d620451d20693a4fdfc969928b8eb8fd
Ben Boeckel <ben.boeckel@kitware.com>
2012-08-06 21:55:25 -0400

    Fix pull merge commit message

--- prepare-commit-msg.d/10-reject-pull-merge.sh
+++ prepare-commit-msg.d/10-reject-pull-merge.sh
@@ -1,5 +1,5 @@
 head -n 1 "$1" | grep -q -e "Merge branch \'.*\' of .*" &&
-	die "Non-fast forward merges when pulling is not allowed.\n" \
-	    "Pull then remerge or set branch.<branch>.rebase = true."
+	die "Non-fast forward merges when pulling is not allowed.
+Pull then remerge or set branch.<branch>.rebase = true."
 
 :
