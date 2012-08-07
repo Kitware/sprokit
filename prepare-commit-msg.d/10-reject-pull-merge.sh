c498551d43223ac80595af9aab4f443a4d9eb04d
Ben Boeckel <ben.boeckel@kitware.com>
2012-08-06 21:58:07 -0400

    Fix pull merge detection

--- prepare-commit-msg.d/10-reject-pull-merge.sh
+++ prepare-commit-msg.d/10-reject-pull-merge.sh
@@ -1,4 +1,4 @@
-head -n 1 "$1" | grep -q -e "Merge branch \'.*\' of .*" &&
+head -n 1 "$1" | grep -q -e '^Merge branch .* of .*$' &&
 	die "Non-fast forward merges when pulling is not allowed.
 Pull then remerge or set branch.<branch>.rebase = true."
 
